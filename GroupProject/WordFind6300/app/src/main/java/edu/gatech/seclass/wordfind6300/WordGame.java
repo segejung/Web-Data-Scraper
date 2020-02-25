fefcf6d050091076cc5ed1b868f09f86d08c89ed
Roger Kiew <yuichiu416@gmail.com>
2020-02-24 23:59:35 -0600

    implemented weight alg

--- GroupProject/WordFind6300/app/src/main/java/edu/gatech/seclass/wordfind6300/WordGame.java
+++ GroupProject/WordFind6300/app/src/main/java/edu/gatech/seclass/wordfind6300/WordGame.java
@@ -2,8 +2,10 @@ package edu.gatech.seclass.wordfind6300;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.Random;
 import java.util.Collections;
 
@@ -58,12 +60,54 @@ public class WordGame extends AppCompatActivity {
 
     }
 
-    public void generateRandom()
-    {
+    public void generateRandom(){
         list = new ArrayList<>();
+        // A dummy map to simulate a weighter map for now ------------------
+        Map<Character, Integer> dummyMap = new HashMap();
+        dummyMap.put('A', 1);
+        dummyMap.put('B', 1);
+        dummyMap.put('C', 1);
+        dummyMap.put('D', 1);
+        dummyMap.put('E', 1);
+        dummyMap.put('D', 555);
+        dummyMap.put('E', 1);
+        dummyMap.put('F', 1);
+        dummyMap.put('G', 1);
+        dummyMap.put('H', 1);
+        dummyMap.put('I', 1);
+        dummyMap.put('J', 1);
+        dummyMap.put('K', 1);
+        dummyMap.put('L', 1);
+        dummyMap.put('M', 1);
+        dummyMap.put('N', 1);
+        dummyMap.put('O', 1);
+        dummyMap.put('P', 1);
+        dummyMap.put('Q', 1);
+        dummyMap.put('R', 1);
+        dummyMap.put('S', 1);
+        dummyMap.put('T', 1);
+        dummyMap.put('U', 1);
+        dummyMap.put('V', 1);
+        dummyMap.put('W', 1);
+        dummyMap.put('X', 1);
+        dummyMap.put('Y', 1);
+        dummyMap.put('Z', 1);
 
-        String consonants = "BCDFGHJKLMNPQRSTVWXYZ";
-        String vowels = "AEIOU";
+        String consonants = "";
+        String vowels = "";
+
+        for(Character c : dummyMap.keySet()){
+            for (int i = 0; i < dummyMap.get(c); i++) {
+                String C = Character.toString(c);
+                if ("AEIOU".indexOf(C) >= 0) {
+                    vowels += C;
+                } else {
+                    consonants += C;
+                }
+            }
+        }
+
+        // end of dummy map-------------------------------------------------
 
         int consonantCount = 0;
         int vowelCount = 0;
@@ -73,15 +117,18 @@ public class WordGame extends AppCompatActivity {
 
         Random r = new Random();
 
-        for (int i = 0; i < consonantCount; i++)
-        {
-            list.add(Character.toString(consonants.charAt(r.nextInt(consonants.length())))); // Add consonant
+        for (int i = 0; i < consonantCount; i++){
+            char c = consonants.charAt(r.nextInt(consonants.length()));
+            if(c == 'Q'){
+                list.add("Qu");
+            } else{
+                list.add(Character.toString(c)); // Add consonant
+            }
         }
 
         for (int i = 0; i < vowelCount; i++) {
             list.add(Character.toString(vowels.charAt(r.nextInt(vowels.length())))); // Add vowel
         }
         Collections.shuffle(list);
-
     }
 }
